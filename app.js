f9e5a7aa5f6c36dc8d315064564bc35678235b22
nishanthkarthik <nishanthkarthik@live.com>
2016-03-26 13:31:55 +0530

    added user model

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var credentials = require('./env.config.js');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 
 var app = express();
+mongoose.connect(credentials.mongo.url);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
