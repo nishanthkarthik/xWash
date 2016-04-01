38b6ff4d7b2131501236cb160b9508b3e200472b
nishanthkarthik <nishanthkarthik@live.com>
2016-04-01 16:26:17 +0530

    added handler for reserve

--- routes/reserver.js
+++ routes/reserver.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var washer = require('../models/washer');
+var moment = require('moment');
 
 var isAuthenticated = function(req, res, next) {
     if (req.isAuthenticated())
         return next();
     res.redirect('/');
+}
+
+//validate + Date = valiDate
+function valiDate(data, done) {
+    if (!(('washer' in data) && ('date' in data) && ('from' in data) && ('to' in data))) {
+        done('One or more inputs missing');
+        return;
+    } else {
+        done(null, 'samplekey');
+    }
 }
 
 module.exports = function(passport) {
@@ -18,7 +29,16 @@ module.exports = function(passport) {
 
     router.post('/', isAuthenticated, function(req, res) {
         console.log(req.body);
-        res.redirect('/home');
+        valiDate(req.body, function(err, key) {
+            if (err) {
+                console.log(err);
+                req.flash('message', err)
+                res.redirect('/home');
+            } else {
+                req.flash('message', 'Key for Reservation - ' + key);
+                res.redirect('/home');
+            }
+        });
     });
 
     return router;
