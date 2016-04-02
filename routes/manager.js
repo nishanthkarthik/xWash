883dcbb6af3ceacb377d8f7b9fb549b5045d0f3b
nishanthkarthik <nishanthkarthik@live.com>
2016-04-02 18:33:50 +0530

    added slot display

---
+++ routes/manager.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var moment = require('moment');
+var router = express.Router();
+var washer = require('../models/washer');
+
+var isAuthenticated = function(req, res, next) {
+    // if (req.isAuthenticated())
+    //     return next();
+    // res.redirect('/');
+
+    // Allow unauthenticated client machines to check their status
+    return next();
+}
+
+module.exports = function(passport) {
+
+    router.get('/', isAuthenticated, function(req, res) {
+        res.end('specify machine paramater');
+    });
+
+    router.get('/:machineName', function(req, res) {
+        console.log(req.params.machineName + ' machine requested key');
+        washer.findOne({
+            name: req.params.machineName
+        }, function(err, machine) {
+
+            if (!machine) {
+                res.end('machine not found');
+                return;
+            }
+
+            //console.log(machine);
+            
+            t_now = moment();
+            for (i = 0; i < machine.reserver.length; ++i) {
+                var slot_start = moment(machine.reserver[i].starttime);
+                var slot_end = moment(machine.reserver[i].endtime);
+                if (t_now.isSameOrAfter(slot_start) && t_now.isSameOrBefore(slot_end)) {
+                    res.end(machine.reserver[i].key);
+                    return;
+                }
+            }
+
+            res.end('-1');
+
+        });
+    });
+
+    return router;
+}
---
+++ routes/slots.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var moment = require('moment');
+var router = express.Router();
+var washer = require('../models/washer');
+
+var isAuthenticated = function(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/');
+}
+
+module.exports = function(passport) {
+
+    router.get('/:machineName', isAuthenticated, function(req, res) {
+
+        if (!req.params.machineName) {
+            res.redirect('/');
+            return;
+        }
+
+        washer.findOne({
+            name: req.params.machineName
+        }, function(err, machine) {
+            if (err) {
+                console.log(err);
+                req.flash('message', 'Internal Error');
+                res.redirect('/home');
+                return;
+            }
+            if (!machine) {
+                req.flash('message', 'Machine not found');
+                res.redirect('/home');
+            }
+
+            var slotsReserved = [];
+            for (i = 0; i < machine.reserver.length; ++i) {
+                slotsReserved.push({
+                    userName: machine.reserver[i].username,
+                    date: moment(machine.reserver[i].starttime).utc().local().format('Do MMM Y'),
+                    fromTime: moment(machine.reserver[i].starttime).utc().local().format('hh:mm a'),
+                    toTime: moment(machine.reserver[i].endtime).utc().local().format('hh:mm a'),
+                    _sortKey: moment(machine.reserver[i].starttime).unix()
+                });
+            }
+
+            slotsReserved.sort(function(a, b) {
+                return a._sortKey - b._sortKey;
+            });
+
+            res.render('slots', {
+                message: req.flash('message'),
+                machineName: machine.location,
+                slots: slotsReserved
+            });
+
+        });
+
+        // res.render('washer', {
+        //     message: req.flash('message'),
+        // });
+    });
+
+    return router;
+}
